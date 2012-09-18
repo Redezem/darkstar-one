e765786cf78005c7a9df6b52699c97e39c4036f1
Redezem <AtreusRedezem@redezria.net>
2012-09-18 17:15:50 +0800

    further additions to objectlist

---
+++ objectlist.cpp
@@ -0,0 +1,12 @@
+#include <GL/gl.h>
+#include <GL/glu.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+#include "objectlist.h"
+#include "object.h"
+
+GraphicObjectList::GraphicObjectList()
+{
+	
+}
--- objectlist.h
+++ objectlist.h
@@ -4,3 +4,18 @@
 #include <GL/glut.h>
 
 #include "object.h"
+
+class GraphicObjectList //A list of stuff that needs to render n stuff
+{
+	GraphicObject* front, back, cur;
+	public:
+		GraphicObjectList();
+		void draw();
+		void ScaleAll(int);
+		void push(GraphicObject);
+		void animate(int,int);
+	private:
+		int isLast();
+		void next();
+		void reset();
+}
