51575c0a08b8911a3eded1d49654690a4cf22f8d
Redezem <AtreusRedezem@redezria.net>
2012-09-19 13:45:06 +0800

    work on objectlist

--- objectlist.cpp
+++ objectlist.cpp
@@ -5,8 +5,68 @@
 
 #include "objectlist.h"
 #include "object.h"
-
+//warning: Gratuitous use of the heap ahead
 GraphicObjectList::GraphicObjectList()
 {
-	
+	front=NULL;
+	back=NULL;
+	cur=NULL;
+}
+
+void GraphicObjectList::draw()
+{
+}
+
+void GraphicObjectList::push(GraphicObject newObject)
+{
+	if(front==NULL)
+	{
+		front=(GraphicObject*)malloc(sizeof(GraphicObject));
+		memcpy(front, newObject, sizeof(GraphicObject));
+		back=front;
+	}
+	else
+	{
+		*back.next=(GraphicObject*)malloc(sizeof(GraphicObject));
+		memcpy(*back.next, newObject, sizeof(GraphicObject));
+		back=*back.next;
+	}
+}
+
+void GraphicObjectList::ScaleAll(int scaleVal)
+{
+	cur=front;
+	do
+	{
+		*cur.scaleVal=scaleVal;
+		cur=*front.next;
+	}while(cur!=back);
+}
+
+void GraphicObjectList::cut(int number)
+{
+	int i;
+	cur=front;
+	GraphicObject* herpaderp; //too tired. So help me.
+	for(i=0;i!=number;i++) //this stops the one before the one required to be blown up
+	{
+		if(*cur.next!=NULL) //Avoid *ALL* the Segfaults! :D
+		{
+			cur=*cur.next;
+		}
+	}
+	//cool, we're here, now to kill stuff
+	if(*cur.next!=NULL && *cur.*next.next!=NULL) //Avoid *ALL* the Segfaults? D:
+	{
+		herpaderp=*cur.next;
+		*cur.next=*herpaderp.next;
+		free(herpaderp); //Fine I'll be memory friendly, sheesh.
+		herpaderp=NULL;
+	}else{
+		printf("Oi Bruv, someone tried to segfault your system by searching beyond the end point of a list >:(");
+	}
+}
+
+void GraphicObjectList::animate(int animationTick, int animationSpeed)
+{
 }
--- objectlist.h
+++ objectlist.h
@@ -7,15 +7,12 @@
 
 class GraphicObjectList //A list of stuff that needs to render n stuff
 {
-	GraphicObject* front, back, cur;
 	public:
+		GraphicObject* front, back, cur;
 		GraphicObjectList();
 		void draw();
 		void ScaleAll(int);
 		void push(GraphicObject);
+		void cut(int);
 		void animate(int,int);
-	private:
-		int isLast();
-		void next();
-		void reset();
-}
+	}
